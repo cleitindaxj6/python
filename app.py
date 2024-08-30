ec07a3bba25ccc2dc555debf3bee1e6356a6c57c
cleitindaxj6 <134400713+cleitindaxj6@users.noreply.github.com>
2024-08-30 07:46:45 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,52 +1,115 @@
 import os
 
+
+automoveis = [
+    {'nome': 'Modelo A', 'categoria': 'SUV', 'ativo': False},
+    {'nome': 'Modelo B', 'categoria': 'Sedan', 'ativo': True},
+    {'nome': 'Modelo C', 'categoria': 'Hatchback', 'ativo': False}
+]
+
 def exibir_nome_do_programa():
-    print('''     
     
+    print("""
+        
 ████████████████████████████████████████████████████████████████████████
 ██▀▄─██▄─██─▄█─▄─▄─█─▄▄─███▄─▄▄─█▄─▄▄▀█▄─▄▄─█▄─▀█▀─▄█▄─▄█▄─██─▄█▄─▀█▀─▄█
 ██─▀─███─██─████─███─██─████─▄▄▄██─▄─▄██─▄█▀██─█▄█─███─███─██─███─█▄█─██
 ▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀  
-''')
+""")
 
-print('Auto Premium\n')
+def exibir_opcoes():
 
-print('1. Cadastrar Novo Cliente')
-print('2. Listar Todos os Clientes')
-print('3. Ativar Serviço')
-print('4. Sair\n')
+    print("1. Cadastrar novo automóvel")
+    print("2. Listar todos os automóveis")
+    print("3. Alternar estado do automóvel")
+    print("4. Sair\n")
 
+def finalizar_app():
 
-opção_escolhida = int(input('Escolha uma opção: '))
-# opção_escolhida = int(opção_escolhida)   
+    exibir_subtitulo("Finalizar app")
 
-def cadastrar_clientes():
-    print('Você escolheu cadrastrar clientes')
+def voltar_ao_menu_principal():
+   
+    input("\nDigite uma tecla para voltar ao menu principal: ")
+    main()
 
-def listar_clientes():
-    print('Você escolheu listar todos os clientes')
+def opcao_invalida():
+   
+    print("Opção inválida\n")
+    voltar_ao_menu_principal()
 
-def ativar_servico():
-    print('Você escolher mostrar sua publicação')   
+def exibir_subtitulo(texto):
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    linha = '*' * len(texto)
+    print(linha)
+    print(texto)
+    print(linha)
+    print()
 
+def cadastrar_novo_automovel():
+   
+    exibir_subtitulo("Cadastro de novos automóveis")
+    nome_do_automovel = input("Digite o nome do automóvel que deseja cadastrar: ")
+    categoria = input(f"Digite a categoria do automóvel {nome_do_automovel} (por exemplo, SUV, Sedan, Hatchback): ")
+    dados_do_automovel = {'nome': nome_do_automovel, 'categoria': categoria, 'ativo': False}
+    automoveis.append(dados_do_automovel)
+    print(f"O automóvel {nome_do_automovel} foi cadastrado com sucesso!")
+    voltar_ao_menu_principal()
 
-def finalizar_programa():
-    os.system('cls')
-    print('finalizar o app\n')
+def listar_automoveis():
 
+    exibir_subtitulo("Listando automóveis")
+    print(f"{'Nome do Automóvel'.ljust(23)} | {'Categoria'.ljust(20)} | {'Status'}")
+    for automovel in automoveis:
+        nome_automovel = automovel['nome']
+        categoria = automovel['categoria']
+        ativo = "ativado" if automovel['ativo'] else "desativado"
+        print(f" - {nome_automovel.ljust(20)} | {categoria.ljust(20)} | {ativo}")
+    voltar_ao_menu_principal()
 
-if opção_escolhida == 1:
-    print('Cadastrar Novo cliente')
-elif opção_escolhida == 2:
-    print('Listar Todos os clientes')
-elif opção_escolhida == 3:
-    print('Ativar Serviço')
-else:
-    print('finalizar programa')
+def alterar_estado_automovel():
+    
+    exibir_subtitulo('Alterando estado do automóvel')
+    nome_automovel = input("Digite o nome do automóvel que deseja alterar o estado: ")
+    automovel_encontrado = False
+
+    for automovel in automoveis:
+        if nome_automovel == automovel['nome']:
+            automovel_encontrado = True
+            automovel['ativo'] = not automovel['ativo']
+            mensagem = f"O automóvel {nome_automovel} foi ativado com sucesso" if automovel['ativo'] else f"O automóvel {nome_automovel} foi desativado com sucesso"
+            print(mensagem)
+            break
+
+    if not automovel_encontrado:
+        print("O automóvel não foi encontrado")
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+ 
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+        if opcao_escolhida == 1:
+            cadastrar_novo_automovel()
+        elif opcao_escolhida == 2:
+            listar_automoveis()
+        elif opcao_escolhida == 3:
+            alterar_estado_automovel()
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except ValueError:
+        opcao_invalida()
 
 def main():
-    exibir_nome_do_programa()    
+ 
+    os.system('cls' if os.name == 'nt' else 'clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
 if __name__ == '__main__':
     main()
-    
