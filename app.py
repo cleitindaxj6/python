3ff9226ab3bc4d429d14a317dd885c17e12c5ebe
cleitindaxj6 <134400713+cleitindaxj6@users.noreply.github.com>
2024-08-30 10:36:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,113 +1,92 @@
+from time import sleep
 import os
 
+automoveis = [{'nome': 'Fusca', 'categoria': 'Hatch', 'ativo': True}, {'nome': 'Civic', 'categoria': 'Sedan', 'ativo': True}, {'nome': 'Fortuner', 'categoria': 'SUV', 'ativo': True}]
 
-automoveis = [
-    {'nome': 'Modelo A', 'categoria': 'SUV', 'ativo': False},
-    {'nome': 'Modelo B', 'categoria': 'Sedan', 'ativo': True},
-    {'nome': 'Modelo C', 'categoria': 'Hatchback', 'ativo': False}
-]
-
-def exibir_nome_do_programa():
-    
+def nome_programa():
+    print()
     print("""
-        
-████████████████████████████████████████████████████████████████████████
+    ████████████████████████████████████████████████████████████████████████
 ██▀▄─██▄─██─▄█─▄─▄─█─▄▄─███▄─▄▄─█▄─▄▄▀█▄─▄▄─█▄─▀█▀─▄█▄─▄█▄─██─▄█▄─▀█▀─▄█
 ██─▀─███─██─████─███─██─████─▄▄▄██─▄─▄██─▄█▀██─█▄█─███─███─██─███─█▄█─██
-▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀  
-""")
+▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀ 
+        """)
 
 def exibir_opcoes():
-
-    print("1. Cadastrar novo automóvel")
-    print("2. Listar todos os automóveis")
-    print("3. Alternar estado do automóvel")
-    print("4. Sair\n")
+    print('1. Cadastrar Automóvel')
+    print('2. Listar Automóveis')
+    print('3. Ativar/Desativar Automóvel')
+    print('4. Sair\n')
 
 def finalizar_app():
-
-    exibir_subtitulo("Finalizar app")
+    exibir_subtitulo('Finalizando o app...')
 
 def voltar_ao_menu_principal():
-   
-    input("\nDigite uma tecla para voltar ao menu principal: ")
+    input('\nDigite uma tecla para voltar ao menu principal: ')
     main()
 
 def opcao_invalida():
-   
-    print("Opção inválida\n")
+    print('Opção inválida!\n')
     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
-    
-    os.system('cls' if os.name == 'nt' else 'clear')
-    linha = '*' * len(texto)
+    os.system('cls')
+    linha = '*' * 70
     print(linha)
     print(texto)
     print(linha)
     print()
 
 def cadastrar_novo_automovel():
-   
-    exibir_subtitulo("Cadastro de novos automóveis")
-    nome_do_automovel = input("Digite o nome do automóvel que deseja cadastrar: ")
-    categoria = input(f"Digite a categoria do automóvel {nome_do_automovel} (por exemplo, SUV, Sedan, Hatchback): ")
-    dados_do_automovel = {'nome': nome_do_automovel, 'categoria': categoria, 'ativo': False}
-    automoveis.append(dados_do_automovel)
-    print(f"O automóvel {nome_do_automovel} foi cadastrado com sucesso!")
+    exibir_subtitulo('Cadastro de novos Automóveis:')
+    nome_automovel = input('Digite o nome do Automóvel que deseja cadastrar: ')
+    categoria = input(f'Digite a categoria do Automóvel {nome_automovel}: ')
+    dados_automovel = {'nome': nome_automovel, 'categoria': categoria, 'ativo': False}
+    print(f'O automóvel {nome_automovel} foi salvo com sucesso!')
+    automoveis.append(dados_automovel)
     voltar_ao_menu_principal()
 
 def listar_automoveis():
-
-    exibir_subtitulo("Listando automóveis")
-    print(f"{'Nome do Automóvel'.ljust(23)} | {'Categoria'.ljust(20)} | {'Status'}")
+    exibir_subtitulo('Listando automóveis:')
+    print(f'{"AUTOMÓVEL".ljust(22)} | {"CATEGORIA".ljust(20)} | STATUS')
     for automovel in automoveis:
         nome_automovel = automovel['nome']
         categoria = automovel['categoria']
-        ativo = "ativado" if automovel['ativo'] else "desativado"
-        print(f" - {nome_automovel.ljust(20)} | {categoria.ljust(20)} | {ativo}")
+        ativo = 'Ativado' if automovel['ativo'] else 'Desativado'
+        print(f'- {nome_automovel.ljust(20)} | {categoria.ljust(20)} | {ativo}')
     voltar_ao_menu_principal()
 
-def alterar_estado_automovel():
-    
+def alterar_situacao_automovel():
     exibir_subtitulo('Alterando estado do automóvel')
-    nome_automovel = input("Digite o nome do automóvel que deseja alterar o estado: ")
+    nome_automovel = input('Digite o nome do Automóvel que deseja alterar o estado: ')
     automovel_encontrado = False
-
     for automovel in automoveis:
         if nome_automovel == automovel['nome']:
             automovel_encontrado = True
             automovel['ativo'] = not automovel['ativo']
-            mensagem = f"O automóvel {nome_automovel} foi ativado com sucesso" if automovel['ativo'] else f"O automóvel {nome_automovel} foi desativado com sucesso"
+            mensagem = f'O automóvel {nome_automovel} foi ativado com sucesso!' if automovel['ativo'] else f'O automóvel {nome_automovel} foi desativado com sucesso!'
             print(mensagem)
-            break
-
     if not automovel_encontrado:
-        print("O automóvel não foi encontrado")
-
+        print('O automóvel não foi encontrado')
     voltar_ao_menu_principal()
 
 def escolher_opcao():
- 
     try:
-        opcao_escolhida = int(input("Escolha uma opção: "))
+        opcao_escolhida = int(input('Escolha uma opção: '))
         if opcao_escolhida == 1:
             cadastrar_novo_automovel()
         elif opcao_escolhida == 2:
             listar_automoveis()
         elif opcao_escolhida == 3:
-            alterar_estado_automovel()
+            alterar_situacao_automovel()
         elif opcao_escolhida == 4:
             finalizar_app()
-        else:
-            opcao_invalida()
-    except ValueError:
+    except:
         opcao_invalida()
 
 def main():
- 
-    os.system('cls' if os.name == 'nt' else 'clear')
-    exibir_nome_do_programa()
+    os.system('cls')
+    nome_programa()
     exibir_opcoes()
     escolher_opcao()
 
